a1421b529300a8279db3aec004f27359a4346f3b
Daniel Trugman <dtrugman@gmail.com>
2020-12-03 20:44:03 +0200

    Add basic implementation
    Implement basic Benchmark class that is capable of benchmarking every
    given function and return the number of nanoseconds it took.

---
+++ sample/bench_void_function.cpp
@@ -0,0 +1,30 @@
+/*
+ *  Copyright 2016-present Daniel Trugman
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#include <iostream>
+#include <fstream>
+
+#include "benchmark.hpp"
+
+using namespace bm;
+
+int main()
+{
+    std::string input("./sample/lipsum.txt");
+    auto duration = Bench::mark([input]() { std::ifstream fin(input, std::ios::binary); });
+    std::cout << "Took " << duration.count() << "ns" << std::endl;
+    return 0;
+}
